aee6800fdf215163cffe41b1cf02ceb390ca6292
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-03-05 13:03:54 -0600

    agrego autenticacion a los recursos API mediante Sanctum

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('products', App\Http\Controllers\ProductController::class);
-Route::resource('categories', App\Http\Controllers\CategoryController::class);
+Route::resource('products', App\Http\Controllers\ProductController::class)->middleware('auth:sanctum');
+Route::resource('categories', App\Http\Controllers\CategoryController::class)->middleware('auth:sanctum');
