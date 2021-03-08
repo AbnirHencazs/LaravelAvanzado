0aae698cbe10f5196f24aa7edc6f043b38e95d42
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-03-08 12:51:42 -0600

    agrego la columna created_by y defino llave foranea en la tabla products

---
+++ database/migrations/2021_03_08_175000_add_created_by_to_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCreatedByToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('created_by')->default( User::factory()->create([
+                'name' => 'Administrator'
+            ])->id );
+
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('created_by');
+        });
+    }
+}
