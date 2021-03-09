3961a9af9e4dee1489a39ff89ee4ca296052add4
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-03-09 11:40:06 -0600

    pruebo las relaciones establecidas en el modelo Rating

--- tests/Unit/Models/RatingTest.php
+++ tests/Unit/Models/RatingTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Product;
+use App\Models\Rating;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -37,5 +38,18 @@ class RatingTest extends TestCase
         $user2->rate( $product, 10 );
 
         $this->assertEquals( 7.5, $product->averageRating( User::class) );
+    }
+
+    public function test_rating_model()
+    {
+        $user = User::factory()->create();
+        $product = Product::factory()->create();
+
+        $user->rate( $product, 5 );
+
+        $rating = Rating::first();
+
+        $this->assertInstanceOf( Product::class, $rating->rateable );
+        $this->assertInstanceOf( User::class, $rating->qualifier );
     }
 }
