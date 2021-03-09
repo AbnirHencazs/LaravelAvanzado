b7c510affc07f9e7fb22d9419ba7a6b7bde7928a
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-03-09 13:27:19 -0600

    creo nuevo comando para mandar newsletter por correo

---
+++ app/Console/Commands/SendNewsletterCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Notifications\NewsletterNotification;
+use Illuminate\Console\Command;
+
+class SendNewsletterCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     * @param array arreglo de emails opcional al cual se enviaran  las notis
+     */
+    protected $signature = 'send:newsletter {emails?*}'; 
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send an email';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $emails = $this->arguments( 'emails' );
+        $builder = User::query();
+
+        if( isset($emails['emails']) ){
+            $builder->whereIn( 'email', $emails['emails'] );
+        }
+
+        $count = $builder->count();
+
+        if( $count ){
+            $this->output->progressStart( $count );
+
+            User::query()
+                ->whereNotNull( 'email_verified_at' )
+                ->each( function (User $user){
+                        $user->notify(new NewsletterNotification());
+                        $this->output->progressAdvance();
+                    } );
+
+            $this->output->progressFinish();
+            return $this->info( "Se enviaron {$count} correos" );
+        }else{
+            $this->info('No se envió ningun correo');
+        }
+
+    }
+}
