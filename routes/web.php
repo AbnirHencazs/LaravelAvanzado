60ab11e272cc94ea1b0d76211d87920603d07709
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-03-09 13:25:10 -0600

    habilito  rutas necesarias para la verificacion de email

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['verify' => true]); //Nos habilitará todas las rutas necesarias para la verificación de email
