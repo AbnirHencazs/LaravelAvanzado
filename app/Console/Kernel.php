b4a649c46a6d4d697ef38aa9983e961fcd58fc72
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-03-10 11:14:13 -0600

    agendo comandos al kernel de la aplicacion

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendNewsletterCommand;
+use App\Console\Commands\SendVerifyEmailCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        SendNewsletterCommand::class,
+        SendVerifyEmailCommand::class
     ];
 
     /**
@@ -24,7 +27,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')->hourly();
+        $schedule->call( function(){
+            echo "hola";
+        } );
+
+        $schedule->command(SendNewsletterCommand::class)->mondays();
+        $schedule->command(SendVerifyEmailCommand::class)->daily();
     }
 
     /**
