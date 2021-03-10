1fe563fb53d4f62380aaa5ab342b8b49b492f209
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-03-10 11:34:12 -0600

    disparamos el evento en el metodo rate del Trait CanRate

--- app/Utils/CanRate.php
+++ app/Utils/CanRate.php
@@ -2,6 +2,7 @@
 
 namespace App\Utils;
 
+use App\Events\ModelRated;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -74,6 +75,8 @@ trait CanRate
             'score' => $score,
             'rateable_type' => get_class($model)
         ]);
+
+        event( new ModelRated( $this, $model, $score ) ); //disparamos el evento, hace falta que alguien lo escuche
 
         return true;
     }
