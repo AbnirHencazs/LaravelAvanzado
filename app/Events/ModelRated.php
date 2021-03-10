4cae0b5692ea18d818a5c51ad8acbee73f6f6b11
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-03-10 11:33:23 -0600

    creamos evento que se dispara cuando se califica un producto y guarda el calificador, el producto y el score

--- app/Events/ModelRated.php
+++ app/Events/ModelRated.php
@@ -7,25 +7,54 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class ModelRated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-
+    public $qualifier;
+    public $rateable;
+    public $score;
     /**
      * Create a new event instance.
-     *
+     * 
+     * @param Model el modelo del calificador
+     * @param Model el modelo que fue calificado
+     * @param float valor de la calificación
      * @return void
      */
-    public function __construct()
+    public function __construct( Model $qualifier, Model $rateable, float $score )
     {
-        //
+        //Vamos a indicar los objetos que intervienen dentro de mi modelo
+        $this->qualifier = $qualifier;
+        $this->rateable = $rateable;
+        $this->score = $score;
+    }
+
+    /**
+     * Creamos getters para las propiedades asignadas en el constructor
+     * Los obtendremos cada vez que el evento se transmita
+     */
+    public function getRateable(): Model
+    {
+        return $this->rateable;
+    }
+
+    public function getQualifier(): Model
+    {
+        return $this->qualifier;
+    }
+
+    public function getScore(): float
+    {
+        return $this->score;
     }
 
     /**
      * Get the channels the event should broadcast on.
+     * sirve para cuando tenemos mensajes de broadcasting
      *
      * @return \Illuminate\Broadcasting\Channel|array
      */
